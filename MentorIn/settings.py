491f89fd7de9a16d2777e42a531175a5ccab6519
Mai Thanh Xuan <xuan.maithanh@outlook.com>
2018-06-15 06:39:03 +0700

    - Update database for Heroku

--- MentorIn/settings.py
+++ MentorIn/settings.py
@@ -147,7 +147,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-# Heroku: Update database configuration from $DATABASE_URL.
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
